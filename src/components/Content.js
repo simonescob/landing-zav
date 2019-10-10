40526ae014d44dd8ddb6ec4c378fc5ac48343173
Simón Escobar <snsimonescobar@gmail.com>
2019-10-10 12:34:29 -0500

    El texto se esta consumiendo desde la api, app.css modificado y content.css creado

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import img from '../img/Trazado compuesto.svg';
 
 class Content extends Component {
-	// state = {  }
+	state = { 
+		elements: [] 
+	}
+	
+	componentDidMount(){
+
+		axios.get("http://localhost:4545/api/v1/text/")
+		.then((res)=>{
+			let result = res.data
+			this.setState({
+				elements: result
+			})
+		})
+		.catch(console.log);
+
+	}
+
 	render() { 
 		return ( 
 			<div className="content">
-				<h1>Aquí tengo que consumir el text de una API</h1>
+
+				{/* <div className="content-img"></div> */}
+				<img className="content-img" src={img}/>
+
+				<div className="ContentBox">
+				{this.state.elements.map((items, key) => <div key={key}>
+					<h1 className="ContentTitle" >{items.title}</h1>
+					<ul>
+					{items.items.map((text, key) => {
+						return <li key={key}>
+							<span className="icon-check"></span>{text}
+						</li>
+					})}
+					</ul>
+				</div>)}
+				</div>
+
 			</div>
 		);
 	}
