290f09bda5313368e4b28d563e6350f8421e27b8
Simón Escobar <snsimonescobar@gmail.com>
2019-10-09 22:35:55 -0500

    Diseño del formulario casi listo, faltan pcos detalles y que funcione

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,8 +4,45 @@ class Form extends Component {
 	// state = {  }
 	render() { 
 		return (
-		<div className="">
-			<h2>Aquí debe ir el formulario</h2>
+		<div className="form">
+			<div className="layeranun">
+				<div className="anuncio">Implantes Dentales</div>
+				<div>desde</div>
+				<div className="signal">$</div>
+				<div id="price"></div>
+				<div className="anuncio">diarios</div>
+				<div className="">*Aplican <a href="#">Términos y Condiciones</a></div>
+			</div>
+
+			<form action="/action_page.php">
+			
+			<div id="TextInForm">Solicita tu COTIZACIÓN GRATIS!</div>
+
+			<p id="TextInFormHelp">Campos obligatorios:<span>*</span></p>
+
+			<label>Nombre completo:<span>*</span></label>
+			<input type="text" id="fname" name="firstname" 
+			placeholder="Tu nombre" 
+			required minLength="8" />
+
+
+			<label>Correo:<span>*</span></label>
+			<input type="email" id="email" name="email" placeholder="tu correo" required />
+
+			<label htmlFor="number">Número de teléfono:<span>*</span>	</label>
+			<input type="number" name="number" id="number" placeholder="número de teléfonos"/>
+
+			<label htmlFor="checkbox" id="checkboxl">
+				<input type="checkbox" name="checkbox" id="checkbox" required/>
+				Acepto las <strong><a href="#">  Políticas de Datos Personales y Privacidad</a></strong>. 
+				Sujeto a valoración odontológica.
+			</label>
+
+			<div className="btnlayer">
+				<input type="submit" className="btnSend" value="Solicitar"/>
+			</div>
+			
+			</form>
 		</div>
 		);
 	}
