4c057422dc8abbc22fc453e8d7a8019b388d12cc
Simón Escobar <snsimonescobar@gmail.com>
2019-10-11 00:15:53 -0500

    Formulario funcionando, guarda la informacion en la base de datos y envia el correo

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const API_PATH = "http://localhost/lading-zav/action.php";
 
 class Form extends Component {
-	// state = {  }
+	state = {
+		name: '',
+		email: '',
+		number: '',
+	}
+
+	handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+	}
+	
+	handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(this.state);
+
+		axios({
+			method: 'post',
+			url: `${API_PATH}`,
+			headers: { 'content-type': 'application/json' },
+			data: this.state
+		})
+			.then(result => {
+				alert("listoooo")
+				// this.setState({
+				// 	mailSent: result.data.sent
+				// })
+			})
+			.catch(error => this.setState({ error: error.message }));
+
+	}
+
 	render() { 
 		return (
 		<div className="form">
@@ -14,23 +52,34 @@ class Form extends Component {
 				<div className="">*Aplican <a href="#">Términos y Condiciones</a></div>
 			</div>
 
-			<form action="/action_page.php">
+			<form onSubmit={this.handleSubmit}>
 			
 			<div id="TextInForm">Solicita tu COTIZACIÓN GRATIS!</div>
 
 			<p id="TextInFormHelp">Campos obligatorios:<span>*</span></p>
 
 			<label>Nombre completo:<span>*</span></label>
-			<input type="text" id="fname" name="firstname" 
+			<input type="text" id="name" name="name" 
 			placeholder="Tu nombre" 
-			required minLength="8" />
+			required minLength="8" 
+			onChange={this.handleInputChange}/>
 
 
 			<label>Correo:<span>*</span></label>
-			<input type="email" id="email" name="email" placeholder="tu correo" required />
+			<input type="email" 
+			id="email" 
+			name="email" 
+			placeholder="tu correo" 
+			minLength="8" required 
+			onChange={this.handleInputChange} />
 
 			<label htmlFor="number">Número de teléfono:<span>*</span>	</label>
-			<input type="number" name="number" id="number" placeholder="número de teléfonos"/>
+			<input type="number" 
+			name="phone" 
+			id="number" 
+			placeholder="número de teléfonos" 
+			minLength="8" 
+			onChange={this.handleInputChange}/>
 
 			<label htmlFor="checkbox" id="checkboxl">
 				<input type="checkbox" name="checkbox" id="checkbox" required/>
