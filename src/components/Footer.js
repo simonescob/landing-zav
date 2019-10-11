06d6a51a2df5eb32bd2e683ebdcf283667f2e0e3
Simón Escobar <snsimonescobar@gmail.com>
2019-10-11 10:20:21 -0500

    footer ajustado

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import "../css/footer.css"
 
 function Footer(params) {
 	return(
 		<div className="footer">
-			{/* <h1>This is the footer</h1> */}
+			<h4>Copyright © 2018 - Sonría</h4>
+
+			<div className="otherText">
+				<div>Llamanos Gratis <br/> desde tu celular</div>
+				<h1>#262</h1>
+			</div>
 		</div>
 	);
 }
